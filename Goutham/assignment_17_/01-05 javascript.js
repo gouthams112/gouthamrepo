089e2ba572ec6bdbabd8f2d6b75ad1f4e13b0094
gouthams112 <somarajugoutham112@gmail.com>
2023-06-03 15:53:55 +0530

    new file

---
+++ Goutham/assignment_17_/01-05 javascript.js
@@ -0,0 +1,133 @@
+
+
+console.log("Assginment Operators")
+
+
+//Addition Assignment Operator
+console.log("Addition Assignment Operator")
+console.log("var a = 5")
+var a=5;
+console.log("var a +=10, a+10")
+a += 10
+console.log(a)
+//Subtrction Assignment Operator
+console.log("Subtrction Assignment Operator")
+console.log("var b = 4")
+var b = 4
+console.log(" b -=6, b-6")
+b -= 6
+console.log(b)
+//Multiplicative Assignment Operator
+console.log("Multiplicative Assignment Operator")
+console.log("var e = 5")
+var e=5
+console.log("e *= 3, e*3")
+e *= 3
+console.log(e)
+//Division Assignment Operator
+console.log("Division Assignment Operator")
+console.log("var c =3")
+var c= 3
+console.log("c /= 8, c/8")
+c /=8
+console.log(c)
+// Module Assignment Operator
+console.log("Module Assignment Operator")
+console.log("var d =20")
+var d= 20
+console.log("d %= 3, it will show reminder")
+d %=3
+console.log(d)
+//Exponentation Assignment Operator
+console.log("Exponentation Assignment Operator")
+console.log("var f = 5")
+var f=5
+console.log("f **= 5, it is that 'f' power 5")
+f **=5
+console.log(f)
+
+
+console.log("ARTHEMATIC OPERATORS")
+//Additon
+console.log("Additon")
+console.log("var a = 1,var b = 2")
+var a = 1
+var b = 2
+console.log("var d = a+b")
+var d = a+b
+console.log(d)
+//Subtraction
+console.log("Subtraction")
+console.log("var e = 4,var f = 5")
+var e = 4
+var f = 5
+console.log("var h = e-f")
+var h = e-f
+console.log(h)
+//Multiplication
+console.log("Multiplication")
+console.log("var i = 7,var j = 8")
+var i = 7
+var j = 8
+console.log("var k = i*j")
+var k = i*j
+console.log(k)
+//Division
+console.log("Division")
+console.log("var l = 10,var m = 2")
+var l = 10
+var m = 2
+console.log("var n = l/m")
+var n = l/m
+console.log(n)
+//Exponentation
+console.log("Exponentation")
+console.log("var o = 3,var p = 4")
+var o = 3
+var p = 4
+console.log("var r = o**p")
+var r = o**p
+console.log(r)
+//Modulus
+console.log("Modulus")
+console.log("var s = 30,var t = 10")
+var s = 30
+var t = 10
+console.log("var u = s%t")
+var u = s%t
+console.log(u)
+
+
+//Comparsion Operators
+console.log("COMPARSION OPERATORS")
+console.log("var a =50,var b = 90")
+var a =50
+var b = 90
+console.log("a>b, a is greater than b")
+console.log(a>b)
+console.log("a<b, a is less than b")
+console.log(a<b)
+console.log("a!=b, a is not equal to b")
+console.log(a!=b)
+console.log("a>=b, a is greater or equal to b")
+console.log(a>=b)
+console.log("a<=b, a is lessthan or equal to b")
+console.log(a<=b)
+console.log("a==b, a is equal to b")
+console.log(a==b)
+console.log("a===b, to check the datatype")
+console.log(a===b)
+
+//LOGICAL OPERATORS
+console.log("LOGICAL OPERATORS --'AND','NOT','OR'")
+console.log("var a =true,var b = false")
+var a =true
+var b = false
+console.log("a&&b, 'AND'--both are true then a 'AND' b is true, else false")
+console.log(a&&b)
+console.log("a||b, 'NOT'--any of a or b is true then a 'OR' is true, else false ")
+console.log(a||b)
+console.log("!a, 'OR'--if a is true then 'a NOT' is false")
+console.log(!a)
+console.log("!b, 'OR'--if b is false then 'b NOT' is true")
+console.log(!b)
---
+++ Goutham/assignment_18_/03-06 Assg1.js
@@ -0,0 +1,78 @@
+console.log("Assigment1");
+
+
+//If given number is even, then print "it is even number"
+//If given number is odd, then print "it is odd number"
+
+let number1 = 20
+var number2 = 25
+console.log(" for Even number");
+
+{
+    if (number1 % 2 == 0)
+    console.log("it is an even number");
+}
+console.log(" for Odd number");
+{
+
+    if (number2 % 2 != 0)
+    console.log("it is an odd number");
+}
+
+console.log("Assignment2");
+//IF your age is above 18, then you can vote, else you cannot vote
+let name = "Goutham"
+let age = 22
+{
+    if (age >= 18)
+    console.log("Goutham is eligible to vote");
+    else
+    console.log("Goutham is not eligible to vote");
+}
+
+console.log("Assignment3");
+//IF your age is above 21 for male, 18 for female then you can marry
+let malename = "raju"
+let gender = "male"
+let maleage = 20
+{
+    if (maleage >= 21 && gender == male)
+    console.log("raju can marry");
+    else
+    console.log("raju cannot marry");
+}
+
+let fname = "rani"
+var genderr = "she"
+var fage = 19
+{
+    if (fage >=18 && genderr == "she")
+    console.log("rani can marry");
+
+    else
+    console.log("rani cannot marry");
+}
+
+console.log("Assignment4");
+//vowels - a, e, i,o ,u. if vowels, then print "it is a vowel" in console.
+
+let o = "hot"
+{
+    if (o == "hot")
+    console.log(" O is a vowel");
+}
+
+console.log("Assignment5");
+//Use type of operator and if variable is string, then log it as "this is a string". If it is a number, log it as "this is a number". ( do same for other datatypes) 
+
+let Name = "ram"
+{
+    if ( Name == "ram")
+    console.log("This is a string");
+}
+
+let number = 123
+{
+    if ( number==123)
+    console.log("this is a number");
+}
